6c65e0e5caf1aeaafebf6757ae23663ca74f1e01
rizwanali1493 <alishakir1493@gmail.com>
2021-08-26 18:02:28 +0500

    first commit

--- mongoose.js
+++ mongoose.js
@@ -19,3 +19,13 @@ mongoose
 //   })
 //   .then(() => console.log("Connect To mongoDB...."))
 //   .catch((err) => console.error("could not connect to mongo", err));
+//   const mongoose = require("mongoose");
+
+// mongoose
+//   .connect(process.env.CONNECTURI_Live, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("Connect To mongoDB...."))
+//   .catch((err) => console.error("could not connect to mongo", err));
